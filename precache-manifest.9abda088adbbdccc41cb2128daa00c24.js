2b80b22e8ceb4da256ddab7c0496790617403bc7
dibyajyotiranjan <73651507+dibyajyotiranjan@users.noreply.github.com>
2020-10-30 22:52:21 +0530

    Updates

---
+++ precache-manifest.9abda088adbbdccc41cb2128daa00c24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "546c25a212f358800a4e",
+    "url": "/.github.io/drr/static/css/main.c93feca5.chunk.css"
+  },
+  {
+    "revision": "546c25a212f358800a4e",
+    "url": "/.github.io/drr/static/js/main.ed3d6bed.chunk.js"
+  },
+  {
+    "revision": "605530975b263501923b",
+    "url": "/.github.io/drr/static/js/runtime~main.46fa3c6f.js"
+  },
+  {
+    "revision": "7cbf73e0807e69fd950a",
+    "url": "/.github.io/drr/static/js/2.12e3c55b.chunk.js"
+  },
+  {
+    "revision": "73f683996374a60fd4a8",
+    "url": "/.github.io/drr/static/js/3.83787b9b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/.github.io/drr/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e92b22d0ac5be8d9cca93eef034834ba",
+    "url": "/.github.io/drr/index.html"
+  }
+];
